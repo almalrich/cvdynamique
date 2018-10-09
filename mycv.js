1f4c513c8ec361538595fbd9fa696ef43cca616a
almalrich <seb-bastos16@live.fr>
2018-10-09 16:51:45 +0200

    cvdynamique

---
+++ mycv.js
@@ -0,0 +1,17 @@
+fonction invisible (){
+    if(document.getElementById("bout").style.display=="none"){
+}
+(document.getElementById("bout").style.display=="block");
+    else{
+    document.getElementById("bout")
+        .Style.display="none";
+    }
+}
+
+
+
+
+
+
+
+
